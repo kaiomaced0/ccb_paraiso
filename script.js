59f6ecaafc33477a8c2d4485331587efba0c231e
kaiomaced0 <kaiomm2000@gmail.com>
2024-02-11 22:33:07 -0300

    alteracao css

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-const btn = document.querySelector('.btn');
-const icon = document.querySelector('.btn__icon');
-const body = document.querySelector('body');
-
 document.addEventListener('DOMContentLoaded', function() {
-    
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.body;
 
-    themeToggle.onclick = function() {
+    themeToggle.addEventListener('click', function() {
         body.classList.toggle('dark-theme');
         body.classList.toggle('light-theme');
-    }
-
-    body.classList.add('dark-theme');
+    });
 });
